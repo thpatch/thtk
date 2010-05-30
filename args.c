caa91dcff277e241aec6da7f60ca4e8e8f6e18ad
xarnonymous <xarnonymous@gmail.com>
2010-05-31 00:10:02 +0200

    datpacker: Implement a new argument parsing system.
    --HG--
    extra : rebase_source : 46ca3ce985f7c6c23f8d58050feb84d8e8d508e2

---
+++ args.c
@@ -0,0 +1,81 @@
+/*
+ * Redistribution and use in source and binary forms, with
+ * or without modification, are permitted provided that the
+ * following conditions are met:
+ *
+ * 1. Redistributions of source code must retain this list
+ *    of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce this
+ *    list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+#include <config.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <string.h>
+#include "args.h"
+#include "program.h"
+#include "util.h"
+
+char
+parse_args(int argc, char* argv[], void (*usage)(void), char* commands, char* options, unsigned int* version)
+{
+    char command;
+    char* argp;
+
+    /* TODO: Some kind of check here first. */
+    argv0 = util_shortname(argv[0]);
+
+    if (argc < 2) {
+        usage();
+        return 0;
+    }
+
+    command = argv[1][0];
+
+    if (!command) {
+        fprintf(stderr, "%s: command missing\n", argv0);
+        return 0;
+    }
+
+    if (!strchr(commands, command)) {
+        fprintf(stderr, "%s: unknown command '%c'\n", argv0, command);
+        usage();
+        return 0;
+    }
+
+    for (argp = argv[1] + 1; *argp;) {
+        char* optionp = strchr(options, *argp);
+        if (optionp) {
+            *optionp = ' ';
+            argp++;
+        } else {
+            long int parsed_version = strtol(argp, &argp, 10);
+            if (parsed_version == 0 || parsed_version == LONG_MIN || parsed_version == LONG_MAX) {
+                fprintf(stderr, "%s: unrecognized option '%c'\n", argv0, *argp);
+                usage();
+                return 0;
+            } else
+                *version = parsed_version;
+        }
+    }
+
+    return command;
+}
--- datpacker.c
+++ datpacker.c
@@ -31,6 +31,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include "args.h"
 #include "datpacker.h"
 #include "thdat.h"
 #include "program.h"
@@ -43,18 +44,6 @@ extern const archive_module_t archive_th08;
 extern const archive_module_t archive_th95;
 
 char library_error[LIBRARY_ERROR_SIZE] = { 0 };
-
-static void
-print_usage(void)
-{
-    printf("Usage: %s [OPTION]... ARCHIVE FILE...\n"
-           "OPTION can be:\n"
-           "  -v VERSION  can be 2, 6, 7, 75, 8, 9, 95, 10, 11, 12, or 125, defaults to 125\n"
-           "  -h          display this help and exit\n"
-           "  -V          display version information and exit\n\n"
-           "Additional documentation might be available at <" PACKAGE_URL ">.\n"
-           "Report bugs to <" PACKAGE_BUGREPORT ">.\n", argv0);
-}
 
 static entry_t*
 archive_add_entry(archive_t* archive, FILE* stream, const char* filename, unsigned int flags)
@@ -108,6 +97,20 @@ archive_free(archive_t* archive)
     }
 }
 
+static void
+print_usage(void)
+{
+    printf("Usage: %s COMMAND[OPTION...] [ARCHIVE [FILE...]]\n"
+           "COMMAND can be:\n"
+           "  c  create an archive\n"
+           "  h  display this help and exit\n"
+           "  V  display version information and exit\n"
+           "OPTION can be:\n"
+           "  #  # can be 2, 6, 7, 75, 8, 9, 95, 10, 11, 12, or 125, defaults to the latest\n\n"
+           "Additional documentation might be available at <" PACKAGE_URL ">.\n"
+           "Report bugs to <" PACKAGE_BUGREPORT ">.\n", argv0);
+}
+
 int
 main(int argc, char* argv[])
 {
@@ -116,33 +119,12 @@ main(int argc, char* argv[])
     const archive_module_t* archive_module = NULL;
     unsigned int version = 125;
     int i;
-    int argstart;
+    int mode;
 
-    argv0 = util_shortname(argv[0]);
+    mode = parse_args(argc, argv, print_usage, "chV", "", &version);
 
-    for (i = 1; i < argc; ++i) {
-        if (strcmp(argv[i], "-h") == 0) {
-            print_usage();
-            return 0;
-        } else if (strcmp(argv[i], "-V") == 0) {
-            util_print_version("datpacker", PACKAGE_THDAT_VERSION);
-            return 0;
-        } else if (strcmp(argv[i], "-v") == 0) {
-            ++i;
-            if (i == argc) {
-                print_usage();
-                return 1;
-            }
-            version = strtol(argv[i], NULL, 10);
-        } else {
-            break;
-        }
-    }
-
-    if ((i + 2) > argc) {
-        print_usage();
+    if (!mode)
         return 1;
-    }
 
     switch (version) {
     case 2:
@@ -173,59 +155,74 @@ main(int argc, char* argv[])
         return 1;
     }
 
-    archive = fopen(argv[i], "wb");
-    if (!archive) {
-        fprintf(stderr, "%s: couldn't open %s for writing\n", argv0, argv[i]);
-        return 1;
-    }
-    current_output = argv[i];
+    switch (mode) {
+    case 'h':
+        print_usage();
+        return 0;
+    case 'V':
+        util_print_version("datpacker", PACKAGE_THDAT_VERSION);
+        return 0;
+    case 'c':
+        if (argc < 4) {
+            print_usage();
+            return 1;
+        }
+        archive = fopen(argv[2], "wb");
+        if (!archive) {
+            fprintf(stderr, "%s: couldn't open %s for writing\n", argv0, argv[2]);
+            return 1;
+        }
+        current_output = argv[2];
 
-    private = archive_module->create(archive, version, argc - i - 1);
-    if (!private) {
-        fprintf(stderr, "%s: %s\n", argv0, library_error);
-        fclose(archive);
-        return 1;
-    }
+        private = archive_module->create(archive, version, argc - 3);
+        if (!private) {
+            fprintf(stderr, "%s: %s\n", argv0, library_error);
+            fclose(archive);
+            return 1;
+        }
 
-    argstart = i + 1;
 #pragma omp parallel shared(private)
 #pragma omp for
-    for (i = argstart; i < argc; ++i) {
-        entry_t* entry;
-        FILE* stream = fopen(argv[i], "rb");
-        if (!stream) {
-            fprintf(stderr, "%s: couldn't open %s for reading\n", argv0, argv[i]);
-            fclose(archive);
-            exit(1);
-        }
-        current_input = argv[i];
+        for (i = 3; i < argc; ++i) {
+            entry_t* entry;
+            FILE* stream = fopen(argv[i], "rb");
+            if (!stream) {
+                fprintf(stderr, "%s: couldn't open %s for reading\n", argv0, argv[i]);
+                fclose(archive);
+                exit(1);
+            }
+            current_input = argv[i];
 #pragma omp critical
 {
-        printf("%s...\n", argv[i]);
-        fflush(stdout);
+            printf("%s...\n", argv[i]);
+            fflush(stdout);
 }
 
-        entry = archive_add_entry(private, stream, argv[i], archive_module->flags);
-        if (!entry || archive_module->write(private, entry, stream) == -1) {
-            fprintf(stderr, "%s: %s\n", argv0, library_error);
+            entry = archive_add_entry(private, stream, argv[i], archive_module->flags);
+            if (!entry || archive_module->write(private, entry, stream) == -1) {
+                fprintf(stderr, "%s: %s\n", argv0, library_error);
+                fclose(stream);
+                fclose(archive);
+                archive_free(private);
+                exit(1);
+            }
+
             fclose(stream);
-            fclose(archive);
-            archive_free(private);
-            exit(1);
         }
 
-        fclose(stream);
-    }
+        if (archive_check_duplicates(private) || archive_module->close(private) == -1) {
+            fprintf(stderr, "%s: %s\n", argv0, library_error);
+            fclose(archive);
+            archive_free(private);
+            return 1;
+        }
 
-    if (archive_check_duplicates(private) || archive_module->close(private) == -1) {
-        fprintf(stderr, "%s: %s\n", argv0, library_error);
-        fclose(archive);
         archive_free(private);
+        fclose(archive);
+        return 0;
+    default:
+        /* XXX: This shouldn't happen. */
+        abort();
         return 1;
     }
-
-    archive_free(private);
-    fclose(archive);
-
-    return 0;
 }
