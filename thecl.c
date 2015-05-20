141dea9002c3436c6076c4035b600d7ee22b29cb
VBChunguk <chwo9843@gmail.com>
2015-05-20 18:38:12 +0900

    thdat: Add th15 in usage info

--- thecl.c
+++ thecl.c
@@ -168,7 +168,7 @@ print_usage(void)
            "  d  dump ECL file\n"
            "  V  display version information and exit\n"
            "OPTION can be:\n"
-           "  #  # can be 6, 7, 8, 9, 95, 10, 103 (for Uwabami Breakers), 11, 12, 125, 128, 13, 14, or 143 (required)\n"
+           "  #  # can be 6, 7, 8, 9, 95, 10, 103 (for Uwabami Breakers), 11, 12, 125, 128, 13, 14, 143, or 15 (required)\n"
            "Report bugs to <" PACKAGE_BUGREPORT ">.\n", argv0);
 }
 
