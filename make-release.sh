b047d2a60d3b65a1c8da5b7d3a86047538554fe7
Lymia Aluysia <lymia@lymiahugs.com>
2017-10-31 16:19:55 -0500

    Seperate pdbs out into their own file.

--- make-release.sh
+++ make-release.sh
@@ -9,13 +9,17 @@ releasepath=thtk-bin-$1
 echo Generating $releasepath
 rm -rf $releasepath
 mkdir $releasepath
+rm -rf $releasepath-pdbs
+mkdir $releasepath-pdbs
 
 for i in thanm thecl thdat thmsg; do
   echo $i
   groff -mdoc -Tutf8 $i/$i.1 | perl -pe 's/\e\[?.*?[\@-~]//g' | unix2dos > $releasepath/README.$i.txt
-  cp build/$i/RelWithDebInfo/$i.{exe,pdb} $releasepath/
+  cp build/$i/RelWithDebInfo/$i.exe $releasepath/
+  cp build/$i/RelWithDebInfo/$i.exe $releasepath-pdbs/
 done
-cp build/thtk/RelWithDebInfo/thtk.{dll,pdb} $releasepath/
+cp build/thtk/RelWithDebInfo/thtk.dll $releasepath/
+cp build/thtk/RelWithDebInfo/thtk.pdb $releasepath-pdbs/
 cp "$(cygpath "$VSSDK140Install")/../VC/redist/x86/Microsoft.VC140.OPENMP/vcomp140.dll" $releasepath/
 
 copy_doc() {
@@ -27,3 +31,4 @@ copy_doc() {
 copy_doc COPYING.{bison,flex,libpng,zlib} COPYING README NEWS
 
 zip -r -9 $releasepath.zip $releasepath
+zip -r -9 $releasepath-pdbs.zip $releasepath-pdbs
