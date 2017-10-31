a7418e5a52977df4d415dd1e2c1bba9cf515bec4
Lymia Aluysia <lymia@lymiahugs.com>
2017-10-31 16:20:58 -0500

    Fixed typo.

--- make-release.sh
+++ make-release.sh
@@ -16,7 +16,7 @@ for i in thanm thecl thdat thmsg; do
   echo $i
   groff -mdoc -Tutf8 $i/$i.1 | perl -pe 's/\e\[?.*?[\@-~]//g' | unix2dos > $releasepath/README.$i.txt
   cp build/$i/RelWithDebInfo/$i.exe $releasepath/
-  cp build/$i/RelWithDebInfo/$i.exe $releasepath-pdbs/
+  cp build/$i/RelWithDebInfo/$i.pdb $releasepath-pdbs/
 done
 cp build/thtk/RelWithDebInfo/thtk.dll $releasepath/
 cp build/thtk/RelWithDebInfo/thtk.pdb $releasepath-pdbs/
