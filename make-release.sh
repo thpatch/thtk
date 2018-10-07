e9c4bb9a3e513e49fe7c357213d6201005bb75cd
nmlgc <nmlgc@nmlgc.net>
2018-10-07 20:52:35 +0200

    thstd: Add to release script.

--- make-release.sh
+++ make-release.sh
@@ -12,7 +12,7 @@ mkdir $releasepath
 rm -rf $releasepath-pdbs
 mkdir $releasepath-pdbs
 
-for i in thanm thecl thdat thmsg; do
+for i in thanm thecl thdat thmsg thstd; do
   echo $i
   groff -mdoc -Tutf8 $i/$i.1 | perl -pe 's/\e\[?.*?[\@-~]//g' | unix2dos > $releasepath/README.$i.txt
   cp build/$i/RelWithDebInfo/$i.exe $releasepath/
