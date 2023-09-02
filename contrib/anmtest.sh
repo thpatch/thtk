485e6d4a2d58ba2c1d1d6153ab171b6920c072e3
Egor <egor@opensrc.club>
2023-09-03 02:51:52 +0300

    contrib: add anmtest.sh
    current results (just as the manpage foretold):
    095
    Files /tmp/anmtemp/a/front.anm and /tmp/anmtemp/c/front.anm differ
    Files /tmp/anmtemp/a/front.anm and /tmp/anmtemp/g/front.anm differ
    12
    Files /tmp/anmtemp/a/stgenm02.anm and /tmp/anmtemp/c/stgenm02.anm differ
    Files /tmp/anmtemp/a/stgenm03.anm and /tmp/anmtemp/c/stgenm03.anm differ
    Files /tmp/anmtemp/a/stgenm04.anm and /tmp/anmtemp/c/stgenm04.anm differ
    Files /tmp/anmtemp/a/stgenm05.anm and /tmp/anmtemp/c/stgenm05.anm differ
    Files /tmp/anmtemp/a/stgenm05m.anm and /tmp/anmtemp/c/stgenm05m.anm differ
    Files /tmp/anmtemp/a/stgenm07.anm and /tmp/anmtemp/c/stgenm07.anm differ
    Files /tmp/anmtemp/a/stgenm07m.anm and /tmp/anmtemp/c/stgenm07m.anm differ
    13
    Files /tmp/anmtemp/a/st07menm.anm and /tmp/anmtemp/c/st07menm.anm differ
    19
    Files /tmp/anmtemp/a/ability.anm and /tmp/anmtemp/c/ability.anm differ
    Files /tmp/anmtemp/a/front.anm and /tmp/anmtemp/c/front.anm differ
    Files /tmp/anmtemp/a/pl00f.anm and /tmp/anmtemp/c/pl00f.anm differ
    Files /tmp/anmtemp/a/pl01f.anm and /tmp/anmtemp/c/pl01f.anm differ
    Files /tmp/anmtemp/a/pl02f.anm and /tmp/anmtemp/c/pl02f.anm differ
    Files /tmp/anmtemp/a/pl03f.anm and /tmp/anmtemp/c/pl03f.anm differ
    Files /tmp/anmtemp/a/pl04f.anm and /tmp/anmtemp/c/pl04f.anm differ
    Files /tmp/anmtemp/a/pl05f.anm and /tmp/anmtemp/c/pl05f.anm differ
    Files /tmp/anmtemp/a/pl06f.anm and /tmp/anmtemp/c/pl06f.anm differ
    Files /tmp/anmtemp/a/pl07f.anm and /tmp/anmtemp/c/pl07f.anm differ
    Files /tmp/anmtemp/a/pl08f.anm and /tmp/anmtemp/c/pl08f.anm differ
    Files /tmp/anmtemp/a/pl09f.anm and /tmp/anmtemp/c/pl09f.anm differ
    Files /tmp/anmtemp/a/pl10f.anm and /tmp/anmtemp/c/pl10f.anm differ
    Files /tmp/anmtemp/a/pl11f.anm and /tmp/anmtemp/c/pl11f.anm differ
    Files /tmp/anmtemp/a/pl12f.anm and /tmp/anmtemp/c/pl12f.anm differ
    Files /tmp/anmtemp/a/pl13f.anm and /tmp/anmtemp/c/pl13f.anm differ
    Files /tmp/anmtemp/a/pl14f.anm and /tmp/anmtemp/c/pl14f.anm differ
    Files /tmp/anmtemp/a/pl15f.anm and /tmp/anmtemp/c/pl15f.anm differ
    Files /tmp/anmtemp/a/pl16f.anm and /tmp/anmtemp/c/pl16f.anm differ
    Files /tmp/anmtemp/a/pl17f.anm and /tmp/anmtemp/c/pl17f.anm differ
    Files /tmp/anmtemp/a/pl18f.anm and /tmp/anmtemp/c/pl18f.anm differ
    Files /tmp/anmtemp/a/title.anm and /tmp/anmtemp/c/title.anm differ
    Files /tmp/anmtemp/a/world02.anm and /tmp/anmtemp/c/world02.anm differ

---
+++ contrib/anmtest.sh
@@ -0,0 +1,91 @@
+#!/bin/sh
+# Script to test anm rebuilding
+#
+# This tests two main scenarios:
+# - regular unpacking to separate dirs
+#   (one dir per anm file)
+# - unpacking with -uu option
+#
+# This doesn't test the most common way to extract ANM (no flags, everything in
+# one dir), because that's clearly the wrong way to do it.
+#
+# Summary of directories being created:
+# -uu |ZUN| separate
+# flag|   | dirs
+# ----|---|----------
+# g   | a |   c     e | packed
+#  \  |/ \|  / \   /
+#   \ /   \ /   \ /
+#    f|   |b     d    | unpacked
+#
+# Comparisions being done:
+# a = c (rebuilding)
+# b = d (consistency)
+# c = e (consistency)
+# a = g (rebuilding)
+# The a = c comparision can't be practically done for th19... at least for now.
+script_dir=$(cd "$(dirname "$0")" || exit 1; pwd) || exit 1
+: "${THDAT:=$script_dir/../build/thdat/thdat}"
+: "${THANM:=$script_dir/../build/thanm/thanm}"
+: "${ANMTEMP:=/tmp/anmtemp}"
+if [ ! -d "$DATDIR" ]; then
+    echo "Usage: [THDAT=thdat] [THANM=thanm] [ANMTMP=/tmp/anmtemp] DATDIR=dats $0"
+    exit 1
+fi
+DATDIR=$(cd "$DATDIR" || exit 1; pwd) || exit 1
+#(cd "$DATDIR" || exit 1; sha256sum -c "$script_dir/datsums.txt")
+versionfromfn() {
+    if [ "$1" = "alcostg.dat" ]; then
+        echo 103
+    else
+        v=${1#th}
+        echo "${v%.dat}"
+    fi
+}
+THANMFLAGS=
+for datfile in \
+    th06.dat th07.dat th08.dat th09.dat \
+    th095.dat th10.dat alcostg.dat th11.dat \
+    th12.dat th125.dat th128.dat th13.dat \
+    th14.dat th143.dat th15.dat th16.dat \
+    th165.dat th17.dat th18.dat th185.dat \
+    th19.dat
+do
+    version=$(versionfromfn $datfile)
+    echo "$version"
+    rm -rf "$ANMTEMP"
+    if [ "$datfile" = "th06.dat" ]; then
+        datfiles="紅魔郷CM.DAT 紅魔郷ED.DAT 紅魔郷IN.DAT 紅魔郷MD.DAT 紅魔郷ST.DAT 紅魔郷TL.DAT"
+    else
+        datfiles="$datfile"
+    fi
+    mkdir -p "$ANMTEMP/a" "$ANMTEMP/b" "$ANMTEMP/c" "$ANMTEMP/d" "$ANMTEMP/e" "$ANMTEMP/f" "$ANMTEMP/g"
+    anmfiles=$(cd "$ANMTEMP/a" || exit 1; for i in $datfiles; do "$THDAT" -gx"$version" "$DATDIR/$i" "*anm" >/dev/null; done; ls) || exit 1
+    for i in $anmfiles; do
+        mkdir -p "$ANMTEMP/b/$i"
+        "$THANM" $THANMFLAGS -l"$version" "$ANMTEMP/a/$i" >"$ANMTEMP/b/$i.txt"
+        (cd "$ANMTEMP/b/$i" || exit 1; "$THANM" $THANMFLAGS -x"$version" "$ANMTEMP/a/$i") || exit 1
+    done
+    for i in $anmfiles; do
+        (cd "$ANMTEMP/b/$i" || exit 1; "$THANM" $THANMFLAGS -c"$version" "$ANMTEMP/c/$i" "$ANMTEMP/b/$i.txt") || exit 1
+    done
+    for i in $anmfiles; do
+        mkdir -p "$ANMTEMP/d/$i"
+        "$THANM" $THANMFLAGS -l"$version" "$ANMTEMP/c/$i" >"$ANMTEMP/d/$i.txt"
+        (cd "$ANMTEMP/d/$i" || exit 1; "$THANM" $THANMFLAGS -x"$version" "$ANMTEMP/c/$i") || exit 1
+    done
+    for i in $anmfiles; do
+        (cd "$ANMTEMP/d/$i" || exit 1; "$THANM" $THANMFLAGS -c"$version" "$ANMTEMP/e/$i" "$ANMTEMP/d/$i.txt") || exit 1
+    done
+    for i in $anmfiles; do
+        "$THANM" $THANMFLAGS -uul"$version" "$ANMTEMP/a/$i" >"$ANMTEMP/f/$i.txt"
+        (cd "$ANMTEMP/f" || exit 1; "$THANM" $THANMFLAGS -uux"$version" "$ANMTEMP/a/$i") || exit 1
+    done
+    for i in $anmfiles; do
+        (cd "$ANMTEMP/f" || exit 1; "$THANM" $THANMFLAGS -uuc"$version" "$ANMTEMP/g/$i" "$ANMTEMP/f/$i.txt") || exit 1
+    done
+    diff -rq "$ANMTEMP/a" "$ANMTEMP/c"
+    diff -rq "$ANMTEMP/b" "$ANMTEMP/d"
+    diff -rq "$ANMTEMP/c" "$ANMTEMP/e"
+    diff -rq "$ANMTEMP/a" "$ANMTEMP/g"
+done
